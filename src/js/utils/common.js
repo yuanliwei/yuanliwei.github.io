6c930da16f2783e535347368d3102fb34098d838
yuanliwei <891789592@qq.com>
2016-05-15 15:22:41 +0800

    修改目录
    Signed-off-by: yuanliwei <891789592@qq.com>

--- src/js/utils/common.js
+++ src/js/utils/common.js
@@ -1,29 +1,6 @@
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
-  
-var ckEvent = 'touchend';
-var ckTouchStart = 'touchstart';
-var ckTouchMove = 'touchmove';
-try {
-	ontouchstart;
-} catch (e) {
-	ckEvent = 'click';
-	ckTouchStart = 'click';
-	ckTouchMove = 'click';
-}
 
-var cancleClick = false;
-// 取消浏览器的所有事件，使得active的样式在手机上正常生效
-document.addEventListener('touchstart', function() {
-	cancleClick = false;
-	return false;
-}, false);
-document.addEventListener('touchmove', function() {
-	cancleClick = true;
-}, false);
-document.addEventListener('mousedown', function() {
-	cancleClick = false;
-}, false);
 format.extend(String.prototype);
 String.prototype.trim = function() {
 	return this.replace(/(^\s*)|(\s*$)/g, "");
