08f068a874c458083ee32f9a7fe2e151c0b1af96
yuanliwei <891789592@qq.com>
2016-12-22 10:58:42 +0800

    course list

--- src/json-tool/json-tool.js
+++ src/json-tool/json-tool.js
@@ -1,3 +1,18 @@
+/*
+container container-fluid
+row
+col-xs-12 col-sm-12 col-md-12
+col-xs-offset-4 col-sm-offset-4 col-md-offset-4
+bg-info bg-primary bg-success bg-warning bg-danger
+text-left text-center text-right text-justify text-nowrap
+text-muted text-muted text-primary text-success text-info text-warning text-danger
+img-responsive img-rounded img-circle img-thumbnail
+pull-left pull-right
+center-block clearfix
+show hidden
+text-lowercase text-uppercase text-capitalize
+dl-horizontal
+*/
 function initEvent() {
     var input = $('#input_json_text')[0];
     input.onchange = onChange;
@@ -18,6 +33,7 @@ function onChange() {
       switch (mode) {
         case 1: result = js_beautify(json, {}); break;
         case 2: result = getBookListHtml(json); break;
+        case 3: result = getCourseListHtml(json); break;
       }
     } catch (e) {
       result = e.stack;
@@ -32,6 +48,7 @@ function onChange() {
         });
         break;
       case 2:
+      case 3:
         $('pre code').text('');
         $('#result_content').html(result);
         break;
@@ -41,58 +58,45 @@ function onChange() {
 
 function getBookListHtml(json) {
   var bookTempl = `
-    <div class="container-fluid">
-      <div class="row bg-info">
-        <div class="col-md-2 col-sm-2 col-xs-2">
-          <img class="img-responsive img-rounded" src="{img_url.viewurl}" alt="">
+  <div class="container">
+    <div class="row bg-info">
+      <div class="col-md-2 col-sm-2 col-xs-2">
+        <img class="img-responsive img-rounded" src="{img_url.viewurl}" alt="">
+      </div>
+      <div class="col-md-8 col-sm-8 col-xs-8 bg-success">
+        <div class="row">
+          <dl class="dl-horizontal">
+            <dt>book_name</dt> <dd>{book_name}</dd>
+            <dt>book_id</dt> <dd>{book_id}</dd>
+            <dt>zipfile_url</dt> <dd>{zipfile_url.viewurl}</dd>
+          </dl>
         </div>
-        <div class="col-md-8 col-sm-8 col-xs-8 bg-success">
-          <div class="row">
-            <div class="col-xs-10 col-sm-10 col-md-10 text-left">
-              book_name : {book_name}
-            </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 text-left bg-warning">
-              price : {price}
-            </div>
+        <div class="row">
+          <div class="col-xs-6 col-sm-6 col-md-6 bg-success text-danger">
+            <dl class="dl-horizontal">
+              <dt>price</dt> <dd>{price}</dd>
+              <dt>packType</dt> <dd>{packType}</dd>
+              <dt>version</dt> <dd>{version}</dd>
+            </dl>
           </div>
-          <div class="row">
-            <div class="col-xs-10 col-sm-10 col-md-10 text-left">
-              book_id : {book_id}
-            </div>
-            <div class="col-xs-2 col-sm-2 col-md-2 text-left bg-primary">
-              version : {version}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 text-left">
-              packType : {packType}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-3 col-sm-3 col-md-3 text-left">
-              flag : {flag}
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3 text-left">
-              valid : {valid}
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6 text-left">
-              end_date : {end_date}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 text-left">
-              zipfile_url : {zipfile_url.viewurl}
-            </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 bg-danger text-info">
+            <dl class="dl-horizontal">
+              <dt>flag</dt> <dd>{flag}</dd>
+              <dt>valid</dt> <dd>{valid}</dd>
+              <dt>end_date</dt> <dd>{end_date}</dd>
+            </dl>
           </div>
         </div>
-        <div class="col-md-2 col-sm-2 col-xs-2">
-          col-md-2
-        </div>
-     </div>
-    </div>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-2">
+        col-md-2
+      </div>
+   </div>
+  </div>
 
   `;
   var books = JSON.parse(json);
+  if(typeof books.data != "undefined") books = books.data;
   var results = [];
   books.forEach(function (book) {
     castEndDate(book);
@@ -102,6 +106,64 @@ function getBookListHtml(json) {
     results.push(bookTempl.format(book));
   });
   return results.join('<br>');
+}
+
+function getCourseListHtml(json) {
+  var courseTempl = `
+  <div class="container">
+    <div class="row bg-info">
+      <div class="col-xs-2 col-sm-2 col-md-2">
+        <img class="img-responsive img-rounded" src="{pictureUrl}" alt="">
+      </div>
+      <div class="col-xs-8 col-sm-8 col-md-8 bg-danger">
+        <div class="row">
+         <div class="col-xs-6 col-sm-6 col-md-6">
+           <dl class="dl-horizontal">
+             <dt>courseCode</dt> <dd>{courseCode}</dd>
+             <dt>courseName</dt> <dd>{courseName}</dd>
+             <dt>courseId</dt> <dd>{courseId}</dd>
+             <dt>teacherName</dt> <dd>{teacherName}</dd>
+           </dl>
+         </div>
+         <div class="col-xs-6 col-sm-6 col-md-6">
+           <dl class="dl-horizontal">
+             <dt>createTime</dt> <dd>{createTime}</dd>
+             <dt>joinTime</dt> <dd>{joinTime}</dd>
+             <dt>ownerId</dt> <dd>{ownerId}</dd>
+             <dt>status</dt> <dd>{status}</dd>
+           </dl>
+         </div>
+        </div>
+      </div>
+      <div class="col-md-2 col-sm-2 col-xs-2">
+        col-md-2
+      </div>
+   </div>
+  </div>
+
+  `;
+  var courses = JSON.parse(json);
+  if(typeof courses.data != "undefined"
+      && typeof courses.data.list != "undefined") courses = courses.data.list;
+  var results = [];
+  courses.forEach(function (course) {
+    castCourseVar(course);
+    results.push(courseTempl.format(course));
+  });
+  return results.join('<br>');
+}
+
+function castCourseVar(course) {
+  var createTime = course.createTime;
+  course.createTime = new Date(createTime).Format('yyyy-MM-dd hh:mm:ss');
+  var joinTime = course.joinTime;
+  course.joinTime = new Date(joinTime).Format('yyyy-MM-dd hh:mm:ss');
+  var status = course.status;
+  switch (status) {
+    case 1: course.status = "1 - 当前课程"; break;
+    case 2: course.status = "2 - 以往课程"; break;
+    default:  course.status = status + " - ??课程"; break;
+  }
 }
 
 function castEndDate(book) {
