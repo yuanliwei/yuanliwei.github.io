c8c2b655971ec48f7e14968f08c73f9ec3a9a05c
yuanliwei <891789592@qq.com>
2017-04-12 00:51:30 +0800

    Capture 添加列表过滤功能

--- src/js/list/normal-list.js
+++ src/js/list/normal-list.js
@@ -6,27 +6,69 @@ var NormalList = (function () {
     var item = this.root.find('.item');
     item.remove();
 
-    this.handle = null;
-    this.datas = [];
+    this.handle = null;   // 数据处理器
+    this.rawDatas = [];   // 原始数据
+    this.datas = [];      // 过滤后的数据
+    this.filter = /./;    // 过滤器
+    this.filterStr = ".";
 
+    // 设置数据处理器
     this.setHandle = function (handle_) {
       this.handle = handle_;
     };
 
+    // 设置数据
     this.setDatas = function (datas_) {
       this.root.html('');
-      this.datas = datas_;
+      this.rawDatas = datas_;
+      this.datas = this.filterDatas();
       this.updateList();
     };
 
-    this.append = function (data_) {
-      this.datas.push(data_);
-      var i = this.datas.length - 1;
-      var holder = this.getViewHolder(i);
-      var data = this.datas[i];
-      this.bindData(i, holder, data);
+    // 设置过滤器
+    this.setFilter = function (filter_, ignoreCase_) {
+      if (this.filterStr == filter_) {
+        return;
+      }
+      this.filterStr = filter_;
+      this.filter = new RegExp(filter_);
+      this.filter.ignoreCase = ignoreCase_;
+      this.setDatas(this.rawDatas);
     };
 
+    // 过滤数据
+    this.filterDatas = function () {
+        var datas = [];
+        for (var i = 0; i < this.rawDatas.length; i++) {
+          var data = this.rawDatas[i];
+          if (this.filterData(data)) {
+            datas.push(data);
+          }
+        }
+        return datas;
+    };
+
+    this.filterData = function (data) {
+      if (typeof this.handle.filter == "function") {
+        return this.handle.filter(data, this.filter);
+      } else {
+        return true;
+      }
+    };
+
+    // 添加数据
+    this.append = function (data_) {
+      this.rawDatas.push(data_);
+      if (this.filterData(data_)) {
+        this.datas.push(data_);
+        var i = this.datas.length - 1;
+        var holder = this.getViewHolder(i);
+        var data = this.datas[i];
+        this.bindData(i, holder, data);
+      }
+    };
+
+    // 更新列表
     this.updateList = function () {
       for (var i = 0; i < this.datas.length; i++) {
         var holder = this.getViewHolder(i);
