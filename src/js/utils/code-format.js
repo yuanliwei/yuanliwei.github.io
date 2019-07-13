80c98f2eda07d3c1ec953c27f3eeba64f308a49d
yuanliwei <891789592@qq.com>
2019-07-13 16:49:43 +0800

    update alignComment

--- src/js/utils/code-format.js
+++ src/js/utils/code-format.js
@@ -19,17 +19,17 @@ CodeFormat.alignComment = function (input) {
     var newLines = [];
     var m = /http.?:\/\//;
     lines.forEach(function (item) {
-        if(!m.test(item)){
-            item = item.replace('//', '#sp#//');   
+        if (!m.test(item)) {
+            item = item.replace('//', '#sp#//');
         }
-        var items = item.split('#sp#');
+        var items = item.split('#sp#//');
         var newLine = items[0];
         if (items.length == 2) {
             if (items[0].trim().length == 0) {
-                newLine += items[1];
+                newLine += '// ' + items[1].trim();
             } else {
                 var space = maxLength - items[0].length;
-                newLine += ' '.repeat(space) + items[1];
+                newLine += ' '.repeat(space) + '// ' + items[1].trim();
             }
         }
         newLines.push(newLine);
